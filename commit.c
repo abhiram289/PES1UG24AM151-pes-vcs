1466387f99b92b96f5c1712d5665bc738f5cfb57
Abhiram <174859692+abhiram289@users.noreply.github.com>
2026-04-21 23:17:15 +0530

    Tested files

--- commit.c
+++ commit.c
@@ -224,8 +224,9 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
 
     commit.tree = tree_id;
 
-    if (try_read_head_parent(&commit.parent, &commit.has_parent) != 0)
-        return -1;
+    if (try_read_head_parent(&commit.parent, &commit.has_parent) != 0) {
+    commit.has_parent = 0;  // No parent for initial commit
+}
 
     fill_commit_identity(&commit);
 
